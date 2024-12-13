f25c036c6e653f35958bb35e8457e1aebe904def
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-12-13 15:22:30 +0200

    .

--- src/main/java/com/lms/service/PersonServiceImpl.java
+++ src/main/java/com/lms/service/PersonServiceImpl.java
@@ -1,15 +1,17 @@
 package com.lms.service;
 
-import java.util.*;
 import com.lms.persistence.*;
+
+import java.util.*;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class PersonServiceImpl implements PersonService {
+public class PersonServiceImpl {
 
     Map<Integer, Person> persons = new HashMap<>();
 
-    @Override
+
     public Boolean addPerson(Person p) {
         if (persons.get(p.getId()) != null) {
             return false;
@@ -18,7 +20,7 @@ public class PersonServiceImpl implements PersonService {
         return true;
     }
 
-    @Override
+
     public Boolean deletePerson(int id) {
         if (persons.get(id) == null) {
             return false;
@@ -27,12 +29,12 @@ public class PersonServiceImpl implements PersonService {
         return true;
     }
 
-    @Override
+
     public Person getPerson(int id) {
         return persons.get(id);
     }
 
-    @Override
+
     public List<Person> getAllPersons() {
         Set<Integer> ids = persons.keySet();
         List<Person> p = new ArrayList<>(ids.size());
