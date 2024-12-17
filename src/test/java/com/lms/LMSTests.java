dc1c11aca0e9733df9ab12767a51f15944e51f2a
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2024-12-17 16:42:18 +0200

    .

--- src/test/java/com/lms/LMSTests.java
+++ src/test/java/com/lms/LMSTests.java
@@ -4,35 +4,120 @@ import static com.lms.LMSTestFunctions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LMSTests {
 
+  static String adminEmail = "admin@example.com";
+  static String adminPassword = "password123";
+
+  static String instructorEmail = "instructor@example.com";
+  static String instructorPassword = "password123";
+
+  static String studentEmail = "student@example.com";
+  static String studentPassword = "password123";
+
+  static String adminToken;
+  static String instructorToken;
+  static String studentToken;
+
+  static Boolean instructorCreated = false;
+  static Boolean studentCreated = false;
+
   @Test
   void testSignupAdmin() throws IOException {
-    String adminEmail = "admin@example.com";
-    String adminPassword = "password123";
+    if (adminToken == null) {
+      LMSResponse signUpAdmin = signup(
+        "A01",
+        "John",
+        "Doe",
+        adminEmail,
+        adminPassword,
+        "Admin"
+      );
 
-    LMSResponse signUpAdmin = signup(
-      "A01",
+      // System.out.println(
+      //   signUpAdmin.code == 200 ? "Admin created" : "Admin creation failed"
+      // );
+
+      LMSResponse loginAdmin = login(adminEmail, adminPassword);
+      adminToken = getToken(loginAdmin.body);
+
+      System.out.println("AdminToken: " + adminToken);
+      // the prented text in the debug console
+      assertEquals(200, loginAdmin.code);
+    }
+  }
+
+  @Test
+  void testCreateInstructor() throws IOException {
+    if (adminToken == null) {
+      testSignupAdmin();
+    }
+
+    LMSResponse createInstructor = createUser(
+      adminToken,
+      "I01",
       "John",
       "Doe",
-      adminEmail,
-      adminPassword,
-      "Admin"
+      instructorEmail,
+      instructorPassword,
+      "Instructor"
     );
 
-    System.out.println(
-      signUpAdmin.code == 200 ? "Admin created" : "Admin creation failed"
+    System.out.println("Instructor created: " + createInstructor.code);
+    instructorCreated = true;
+
+    assertEquals(200, createInstructor.code);
+  }
+
+  @Test
+  void testLoginInstructor() throws IOException {
+    if (instructorCreated == false) {
+      testCreateInstructor();
+    }
+
+    LMSResponse loginInstructor = login(instructorEmail, instructorPassword);
+    instructorToken = getToken(loginInstructor.body);
+
+    System.out.println("InstructorToken: " + instructorToken);
+    assertEquals(200, loginInstructor.code);
+  }
+
+  @Test
+  void testCreateStudent() throws IOException {
+    if (adminToken == null) {
+      testSignupAdmin();
+    }
+
+    LMSResponse createStudent = createUser(
+      adminToken,
+      "S01",
+      "John",
+      "Doe",
+      studentEmail,
+      studentPassword,
+      "Student"
     );
 
-    LMSResponse loginAdmin = login(adminEmail, adminPassword);
-    String adminToken = getToken(loginAdmin.body);
+    System.out.println("Student created: " + createStudent.code);
+    studentCreated = true;
+    assertEquals(200, createStudent.code);
+  }
 
-    System.out.println("AdminToken: " + adminToken);
-    // the prented text in the debug console
-    assertEquals(200, loginAdmin.code);
+  @Test
+  void testLoginStudent() throws IOException {
+    if (studentCreated == false) {
+      testCreateStudent();
+    }
+
+    LMSResponse loginStudent = login(studentEmail, studentPassword);
+    studentToken = getToken(loginStudent.body);
+
+    System.out.println("StudentToken: " + studentToken);
+    assertEquals(200, loginStudent.code);
   }
 }
